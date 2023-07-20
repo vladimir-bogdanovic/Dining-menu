20d994a2e7c4396d9df2ba851af4c3b73dfa42ef
vladimir-bogdanovic <vladimirbogdanovic44@gmail.com>
2023-07-20 20:47:24 +0200

    push nove grane na git - test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { RouterModule } from "@angular/router";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { WelcomePageComponent } from "./welcome-page/welcome-page.component";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { MenuPageComponent } from "./menu-page/menu-page.component";
+import { CategoryMealsComponent } from "./menu-page/category-meals/category-meals.component";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { AddNewMealComponent } from './add-new-meal/add-new-meal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomePageComponent,
+    MenuPageComponent,
+    CategoryMealsComponent,
+    AddNewMealComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule,
+    NgbModule,
+    ReactiveFormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
